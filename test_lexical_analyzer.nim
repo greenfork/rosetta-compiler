3fbe55daa54ef2533edc5f73a56a8c274f27710a
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-02-24 01:52:30 +0300

    Remove garbage

--- test_lexical_analyzer.nim
+++ test_lexical_analyzer.nim
@@ -1,5 +1,5 @@
 import unittest
-import typeinfo, streams, re, os, sequtils
+import re, os, sequtils
 
 import lexical_analyzer
 
@@ -9,27 +9,17 @@ proc rmWhitespace(str: string): string =
 proc lex(str: string): string =
   str.tokenize.output
 
-proc dumpToFile(name, str: string) =
-  var f = newFileStream(name, fmWrite)
-  f.write str
-  f.close()
-
 test "lexical analysis with inputs":
   let
     numberOfFiles = 15
     inputFiles: seq[string] = toSeq(0..<numberOfFiles).mapIt("input" & $it & ".txt")
     outputFiles: seq[string] = toSeq(0..<numberOfFiles).mapIt("lexed" & $it & ".txt")
   var
-    inputStream, outputStream: Stream
     inputText, outputText: string
 
   for i in 0..<numberOfFiles:
-    inputStream = openFileStream("examples" / inputFiles[i])
-    outputStream = openFileStream("examples" / outputFiles[i])
-    inputText = inputStream.readAll
-    outputText = outputStream.readAll
-    inputStream.close()
-    outputStream.close()
+    inputText = readFile("examples" / inputFiles[i])
+    outputText = readFile("examples" / outputFiles[i])
 
-    debugEcho "Checking input file " & $i & "..."
+    echo "Checking input file " & $i & "..."
     check inputText.lex.rmWhitespace == outputText.rmWhitespace
