b59574f3247d0057f1f7c9e56fc6ddd6d871dd09
Dmitry Matveyev <greenfork.lists@yandex.com>
2021-06-13 12:29:57 +0600

    Clean up code

--- codegen.zig
+++ codegen.zig
@@ -1,5 +1,236 @@
 const std = @import("std");
-const p = std.debug.print;
+
+pub const CodeGeneratorError = error{OutOfMemory};
+
+pub const CodeGenerator = struct {
+    allocator: *std.mem.Allocator,
+    string_pool: std.ArrayList([]const u8),
+    globals: std.ArrayList([]const u8),
+    bytecode: std.ArrayList(u8),
+
+    const Self = @This();
+    const word_size = @sizeOf(i32);
+
+    pub fn init(
+        allocator: *std.mem.Allocator,
+        string_pool: std.ArrayList([]const u8),
+        globals: std.ArrayList([]const u8),
+    ) Self {
+        return CodeGenerator{
+            .allocator = allocator,
+            .string_pool = string_pool,
+            .globals = globals,
+            .bytecode = std.ArrayList(u8).init(allocator),
+        };
+    }
+
+    pub fn gen(self: *Self, ast: ?*Tree) CodeGeneratorError!void {
+        try self.genH(ast);
+        try self.emitHalt();
+    }
+
+    pub fn genH(self: *Self, ast: ?*Tree) CodeGeneratorError!void {
+        if (ast) |t| {
+            switch (t.typ) {
+                .sequence => {
+                    try self.genH(t.left);
+                    try self.genH(t.right);
+                },
+                .kw_while => {
+                    const condition_address = self.currentAddress();
+                    try self.genH(t.left);
+                    try self.emitByte(.jz);
+                    const condition_address_hole = self.currentAddress();
+                    try self.emitHole(4);
+                    try self.genH(t.right);
+                    try self.emitByte(.jmp);
+                    try self.emitInt(condition_address);
+                    self.insertInt(condition_address_hole, self.currentAddress());
+                },
+                .kw_if => {
+                    try self.genH(t.left);
+                    try self.emitByte(.jz);
+                    const condition_address_hole = self.currentAddress();
+                    try self.emitHole(4);
+                    try self.genH(t.right.?.left);
+                    if (t.right.?.right) |else_tree| {
+                        try self.emitByte(.jmp);
+                        const else_address_hole = self.currentAddress();
+                        try self.emitHole(4);
+                        const else_address = self.currentAddress();
+                        try self.genH(else_tree);
+                        self.insertInt(condition_address_hole, else_address);
+                        self.insertInt(else_address_hole, self.currentAddress());
+                    } else {
+                        self.insertInt(condition_address_hole, self.currentAddress());
+                    }
+                },
+                .assign => {
+                    try self.genH(t.right);
+                    try self.emitByte(.store);
+                    try self.emitInt(self.fetchGlobalsOffset(t.left.?.value.?.string));
+                },
+                .prts => {
+                    try self.genH(t.left);
+                    try self.emitByte(.prts);
+                },
+                .prti => {
+                    try self.genH(t.left);
+                    try self.emitByte(.prti);
+                },
+                .prtc => {
+                    try self.genH(t.left);
+                    try self.emitByte(.prtc);
+                },
+                .string => {
+                    try self.emitByte(.push);
+                    try self.emitInt(self.fetchStringsOffset(t.value.?.string));
+                },
+                .integer => {
+                    try self.emitByte(.push);
+                    try self.emitInt(t.value.?.integer);
+                },
+                .identifier => {
+                    try self.emitByte(.fetch);
+                    try self.emitInt(self.fetchGlobalsOffset(t.value.?.string));
+                },
+                .negate, .not => {
+                    try self.genH(t.left);
+                    try self.emitByte(Op.fromNodeType(t.typ).?);
+                },
+                .add,
+                .multiply,
+                .subtract,
+                .divide,
+                .mod,
+                .less,
+                .less_equal,
+                .greater,
+                .greater_equal,
+                .equal,
+                .not_equal,
+                .bool_and,
+                .bool_or,
+                => try self.genBinOp(t),
+                .unknown => {
+                    std.debug.print("\nINTERP: UNKNOWN {}\n", .{t.typ});
+                    std.os.exit(1);
+                },
+            }
+        }
+    }
+
+    fn genBinOp(self: *Self, tree: *Tree) CodeGeneratorError!void {
+        try self.genH(tree.left);
+        try self.genH(tree.right);
+        try self.emitByte(Op.fromNodeType(tree.typ).?);
+    }
+
+    fn emitByte(self: *Self, op: Op) CodeGeneratorError!void {
+        try self.bytecode.append(@enumToInt(op));
+    }
+
+    fn emitInt(self: *Self, n: i32) CodeGeneratorError!void {
+        var n_var = n;
+        var n_bytes = @ptrCast(*[4]u8, &n_var);
+        for (n_bytes) |byte| {
+            try self.bytecode.append(byte);
+        }
+    }
+
+    fn emitHole(self: *Self, size: usize) CodeGeneratorError!void {
+        var n = size;
+        while (n > 0) : (n -= 1) {
+            try self.bytecode.append(0xaa);
+        }
+    }
+
+    fn insertInt(self: *Self, address: i32, n: i32) void {
+        var i: i32 = 0;
+        var n_var = n;
+        var n_bytes = @ptrCast(*[4]u8, &n_var);
+        while (i < word_size) : (i += 1) {
+            self.bytecode.items[@intCast(usize, address + i)] = n_bytes[@intCast(usize, i)];
+        }
+    }
+
+    fn emitHalt(self: *Self) CodeGeneratorError!void {
+        try self.bytecode.append(@enumToInt(Op.halt));
+    }
+
+    fn currentAddress(self: Self) i32 {
+        return @intCast(i32, self.bytecode.items.len);
+    }
+
+    fn fetchStringsOffset(self: Self, str: []const u8) i32 {
+        for (self.string_pool.items) |string, idx| {
+            if (std.mem.eql(u8, string, str)) {
+                return @intCast(i32, idx);
+            }
+        }
+        unreachable;
+    }
+
+    fn fetchGlobalsOffset(self: Self, str: []const u8) i32 {
+        for (self.globals.items) |global, idx| {
+            if (std.mem.eql(u8, global, str)) {
+                return @intCast(i32, idx);
+            }
+        }
+        unreachable;
+    }
+
+    pub fn print(self: Self) ![]u8 {
+        var result = std.ArrayList(u8).init(self.allocator);
+        var writer = result.writer();
+        try writer.print(
+            "Datasize: {d} Strings: {d}\n",
+            .{ self.globals.items.len, self.string_pool.items.len },
+        );
+        for (self.string_pool.items) |string| {
+            try writer.print("{s}\n", .{string});
+        }
+
+        var pc: usize = 0;
+        while (pc < self.bytecode.items.len) : (pc += 1) {
+            try writer.print("{d:>5} ", .{pc});
+            switch (@intToEnum(Op, self.bytecode.items[pc])) {
+                .push => {
+                    try writer.print("push  {d}\n", .{self.unpackInt(pc + 1)});
+                    pc += word_size;
+                },
+                .store => {
+                    try writer.print("store [{d}]\n", .{self.unpackInt(pc + 1)});
+                    pc += word_size;
+                },
+                .fetch => {
+                    try writer.print("fetch [{d}]\n", .{self.unpackInt(pc + 1)});
+                    pc += word_size;
+                },
+                .jz => {
+                    const address = self.unpackInt(pc + 1);
+                    try writer.print("jz     ({d}) {d}\n", .{ address - @intCast(i32, pc) - 1, address });
+                    pc += word_size;
+                },
+                .jmp => {
+                    const address = self.unpackInt(pc + 1);
+                    try writer.print("jmp    ({d}) {d}\n", .{ address - @intCast(i32, pc) - 1, address });
+                    pc += word_size;
+                },
+                else => try writer.print("{s}\n", .{Op.toString(@intToEnum(Op, self.bytecode.items[pc]))}),
+            }
+        }
+
+        return result.items;
+    }
+
+    fn unpackInt(self: Self, pc: usize) i32 {
+        const arg_ptr = @ptrCast(*[4]u8, self.bytecode.items[pc .. pc + word_size]);
+        var arg_array = arg_ptr.*;
+        const arg = @ptrCast(*i32, @alignCast(@alignOf(i32), &arg_array));
+        return arg.*;
+    }
+};
 
 pub const Op = enum(u8) {
     fetch,
@@ -92,242 +323,6 @@ pub const Op = enum(u8) {
     }
 };
 
-pub const CodeGeneratorError = error{OutOfMemory};
-
-pub const CodeGenerator = struct {
-    allocator: *std.mem.Allocator,
-    string_pool: std.ArrayList([]const u8),
-    globals: std.ArrayList([]const u8),
-    bytecode: std.ArrayList(u8),
-
-    const Self = @This();
-    const word_size = @sizeOf(i32);
-
-    pub fn init(
-        allocator: *std.mem.Allocator,
-        string_pool: std.ArrayList([]const u8),
-        globals: std.ArrayList([]const u8),
-    ) Self {
-        return CodeGenerator{
-            .allocator = allocator,
-            .string_pool = string_pool,
-            .globals = globals,
-            .bytecode = std.ArrayList(u8).init(allocator),
-        };
-    }
-
-    pub fn gen(self: *Self, ast: ?*Tree) CodeGeneratorError!void {
-        try self.genH(ast);
-        try self.emitHalt();
-    }
-
-    pub fn genH(self: *Self, ast: ?*Tree) CodeGeneratorError!void {
-        if (ast) |t| {
-            switch (t.typ) {
-                .sequence => {
-                    try self.genH(t.left);
-                    try self.genH(t.right);
-                },
-                .kw_while => {
-                    const condition_address = self.currentAddress();
-                    try self.genH(t.left);
-                    try self.emitByte(.jz);
-                    const point_to_end_address_hole = self.currentAddress();
-                    try self.emitHole(4);
-                    try self.genH(t.right);
-                    try self.emitByte(.jmp);
-                    try self.emitInt(condition_address);
-                    const end_address = self.currentAddress();
-                    self.insertInt(point_to_end_address_hole, end_address);
-                },
-                .kw_if => {
-                    try self.genH(t.left);
-                    try self.emitByte(.jz);
-                    const point_to_else_address_hole = self.currentAddress();
-                    try self.emitHole(4);
-                    try self.genH(t.right.?.left);
-                    if (t.right.?.right) |else_tree| {
-                        try self.emitByte(.jmp);
-                        const point_to_end_address_hole = self.currentAddress();
-                        try self.emitHole(4);
-                        const else_address = self.currentAddress();
-                        try self.genH(else_tree);
-                        const end_address = self.currentAddress();
-                        self.insertInt(point_to_else_address_hole, else_address);
-                        self.insertInt(point_to_end_address_hole, end_address);
-                    } else {
-                        const end_address = self.currentAddress();
-                        self.insertInt(point_to_else_address_hole, end_address);
-                    }
-                },
-                .assign => {
-                    try self.genH(t.right);
-                    try self.emitByte(.store);
-                    try self.emitInt(self.fetchGlobalOffset(t.left.?.value.?.string));
-                },
-                .prts => {
-                    try self.genH(t.left);
-                    try self.emitByte(.prts);
-                },
-                .prti => {
-                    try self.genH(t.left);
-                    try self.emitByte(.prti);
-                },
-                .prtc => {
-                    try self.genH(t.left);
-                    try self.emitByte(.prtc);
-                },
-                .string => {
-                    try self.emitByte(.push);
-                    try self.emitInt(self.fetchStringOffset(t.value.?.string));
-                },
-                .integer => {
-                    try self.emitByte(.push);
-                    try self.emitInt(t.value.?.integer);
-                },
-                .identifier => {
-                    try self.emitByte(.fetch);
-                    try self.emitInt(self.fetchGlobalOffset(t.value.?.string));
-                },
-                .negate, .not => {
-                    try self.genH(t.left);
-                    try self.emitByte(Op.fromNodeType(t.typ).?);
-                },
-                .add,
-                .multiply,
-                .subtract,
-                .divide,
-                .mod,
-                .less,
-                .less_equal,
-                .greater,
-                .greater_equal,
-                .equal,
-                .not_equal,
-                .bool_and,
-                .bool_or,
-                => try self.genBinOp(t),
-                else => {
-                    std.debug.print("\nINTERP: UNKNOWN {}\n", .{t.typ});
-                    std.os.exit(1);
-                },
-            }
-        }
-    }
-
-    fn genBinOp(self: *Self, tree: *Tree) CodeGeneratorError!void {
-        try self.genH(tree.left);
-        try self.genH(tree.right);
-        try self.emitByte(Op.fromNodeType(tree.typ).?);
-    }
-
-    fn emitByte(self: *Self, op: Op) CodeGeneratorError!void {
-        try self.bytecode.append(@enumToInt(op));
-    }
-
-    fn emitInt(self: *Self, n: i32) CodeGeneratorError!void {
-        // p("emitInt: {d}\n", .{n});
-        var n_var = n;
-        var n_bytes = @ptrCast(*[4]u8, &n_var);
-        for (n_bytes) |byte| {
-            try self.bytecode.append(byte);
-        }
-    }
-
-    fn emitHole(self: *Self, size: usize) CodeGeneratorError!void {
-        var n = size;
-        while (n > 0) : (n -= 1) {
-            try self.bytecode.append(0xaa);
-        }
-    }
-
-    fn insertInt(self: *Self, address: i32, n: i32) void {
-        var i: i32 = 0;
-        var n_var = n;
-        var n_bytes = @ptrCast(*[4]u8, &n_var);
-        while (i < word_size) : (i += 1) {
-            self.bytecode.items[@intCast(usize, address + i)] = n_bytes[@intCast(usize, i)];
-        }
-    }
-
-    fn emitHalt(self: *Self) CodeGeneratorError!void {
-        try self.bytecode.append(@enumToInt(Op.halt));
-    }
-
-    fn currentAddress(self: Self) i32 {
-        return @intCast(i32, self.bytecode.items.len);
-    }
-
-    fn fetchStringOffset(self: Self, str: []const u8) i32 {
-        for (self.string_pool.items) |string, idx| {
-            if (std.mem.eql(u8, string, str)) {
-                return @intCast(i32, idx);
-            }
-        }
-        unreachable;
-    }
-
-    fn fetchGlobalOffset(self: Self, str: []const u8) i32 {
-        for (self.globals.items) |global, idx| {
-            if (std.mem.eql(u8, global, str)) {
-                return @intCast(i32, idx);
-            }
-        }
-        unreachable;
-    }
-
-    pub fn print(self: Self) ![]u8 {
-        var result = std.ArrayList(u8).init(self.allocator);
-        var writer = result.writer();
-        try writer.print(
-            "Datasize: {d} Strings: {d}\n",
-            .{ self.globals.items.len, self.string_pool.items.len },
-        );
-        for (self.string_pool.items) |string| {
-            try writer.print("{s}\n", .{string});
-        }
-
-        var pc: usize = 0;
-        while (pc < self.bytecode.items.len) : (pc += 1) {
-            try writer.print("{d:>5} ", .{pc});
-            switch (@intToEnum(Op, self.bytecode.items[pc])) {
-                .push => {
-                    try writer.print("push  {d}\n", .{self.unpackInt(pc + 1)});
-                    pc += word_size;
-                },
-                .store => {
-                    try writer.print("store [{d}]\n", .{self.unpackInt(pc + 1)});
-                    pc += word_size;
-                },
-                .fetch => {
-                    try writer.print("fetch [{d}]\n", .{self.unpackInt(pc + 1)});
-                    pc += word_size;
-                },
-                .jz => {
-                    const address = self.unpackInt(pc + 1);
-                    try writer.print("jz     ({d}) {d}\n", .{ address - @intCast(i32, pc) - 1, address });
-                    pc += word_size;
-                },
-                .jmp => {
-                    const address = self.unpackInt(pc + 1);
-                    try writer.print("jmp    ({d}) {d}\n", .{ address - @intCast(i32, pc) - 1, address });
-                    pc += word_size;
-                },
-                else => try writer.print("{s}\n", .{Op.toString(@intToEnum(Op, self.bytecode.items[pc]))}),
-            }
-        }
-
-        return result.items;
-    }
-
-    fn unpackInt(self: Self, pc: usize) i32 {
-        const arg_ptr = @ptrCast(*[4]u8, self.bytecode.items[pc .. pc + word_size]);
-        var arg_array = arg_ptr.*;
-        const arg = @ptrCast(*i32, @alignCast(@alignOf(i32), &arg_array));
-        return arg.*;
-    }
-};
-
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -346,8 +341,6 @@ pub fn main() !void {
     };
     defer file_handle.close();
     const input_content = try file_handle.readToEndAlloc(allocator, std.math.maxInt(usize));
-    p("=========\n\n{s}\n=========\n", .{input_content});
-    p("\n\n", .{});
 
     var string_pool = std.ArrayList([]const u8).init(allocator);
     var globals = std.ArrayList([]const u8).init(allocator);
@@ -355,9 +348,7 @@ pub fn main() !void {
     var code_generator = CodeGenerator.init(allocator, string_pool, globals);
     try code_generator.gen(ast);
     const result: []const u8 = try code_generator.print();
-    p("=========\n\n", .{});
     _ = try std.io.getStdOut().write(result);
-    p("\n=========\n", .{});
 }
 
 pub const NodeType = enum {
